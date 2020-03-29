2a36990b5f5996093ffb21288ab8eb0d11c950a4
naimishverma17 <itakgec@gmail.com>
2020-03-29 17:52:51 +0530

    added docler compose

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+const DATABASE = 'mongodb://mymongo:27017/test';
 
 app.get('/', (req, res) => {
     res.send('PUBLIC ROUTE');
@@ -9,6 +11,18 @@ app.get('/', (req, res) => {
 app.get('/login', (req, res) => {
     res.json({message: 'You are logged in'});
 });
+
+mongoose.connect(DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
+    .then(() => {
+        console.log('Connected successfully!');
+    })
+    .catch((err) => {
+        console.log('Connection failed!', err);
+    });
 
 app.listen(8000, () => {
     console.log('***App running on port 8000***');
